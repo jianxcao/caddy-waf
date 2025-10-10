95efcabc271517299cc8beca0d5aa17c04d0034c
drev74 <socmicro@gmail.com>
2025-10-10 18:57:30 +0300

    refactor(tor): add custom tor list url

--- common_test.go
+++ common_test.go
@@ -3,4 +3,5 @@ package caddywaf
 const (
 	geoIPdata  = "GeoLite2-Country.mmdb"
 	googleUSIP = "74.125.131.105"
+	torListURL = "https://cdn.nws.neurodyne.pro/nws-cdn-ut8hw561/waf/torbulkexitlist" // custom TOR list URL for testing
 )
